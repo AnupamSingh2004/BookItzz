289d9f501ce62fc3879e137d7570678f784c72a5
anupam <sanupam2004@gmail.com>
2025-01-26 11:40:52 +0530

    Frontend

---
+++ components/AuthForm.tsx
@@ -0,0 +1,8 @@
+// "use client";
+
+// interface Props<T> {}
+
+const AuthForm = ({ type, schema, defaultValues, onSubmit }s) => {
+  return <div>AuthForm -- {type}</div>;
+};
+export default AuthForm;
