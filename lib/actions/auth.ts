df56d66e035027aec2c4905dfa8b5590bb3cd3cb
anupam <sanupam2004@gmail.com>
2025-03-11 20:17:17 +0530

    auth

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,3 +1,28 @@
 "user server";
 
-const signUp = async (params: AuthCredentials) => { };
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { hash } from "bcryptjs";
+import { eq } from "drizzle-orm";
+
+const signUp = async (params: AuthCredentials) => {
+  const { fullName, email, universityId, password, universityCard } = params;
+
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (existingUser.length > 0) {
+    return { success: false, error: "User already Exists" };
+  }
+
+  const hashedPassword = await hash(password, 10);
+
+  try {
+  } catch (error) {
+    console.log(error, "SignUp error");
+    return { success: false, error: "SignUp error" };
+  }
+};
