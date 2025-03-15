1443a318276222cdb6556c092954b9e32ede4f5d
anupam <sanupam2004@gmail.com>
2025-03-15 10:55:33 +0530

    config updates

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "placehold.co",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "m.media-amazon.com",
-        port: "",
       },
       {
         protocol: "https",
