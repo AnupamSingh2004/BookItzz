0e6879fc9c52984cc48905a167b0fb6f0c2a9191
anupam <sanupam2004@gmail.com>
2025-03-16 19:02:33 +0530

    admin dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 import "@/styles/admin.css";
 import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -13,10 +14,10 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <main className={"flex min-h-screen w-full flex-row"}>
-      <Sidebar />
+      <Sidebar session={session} />
 
       <div className={"admin-container"}>
-        <p>Header</p>
+        <Header session={session} />
         {children}
       </div>
     </main>
